d1b190fd96d610213364a4c11753db06a9cff491
matt <matiasberrueta@gmail.com>
2017-12-27 18:43:31 -0300

    Added missing file

---
+++ lib/statistic_calcs/helpers/math.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module StatisticCalcs
+  module Helpers
+    class Math
+      def self.factorial(value)
+        # return factorial_aprox(value) if value.is_a?(Float)
+        return GSL::Sf.gamma(value + 1) if value.is_a?(Float)
+        GSL::Sf.fact(value)
+      end
+
+      def self.euler
+        GSL::M_EULER
+      end
+
+      def self.pi
+        GSL::M_PI
+      end
+
+      def self.e
+        GSL::M_E
+      end
+        
+      # better value with gamma
+      # def factorial_aprox(value)
+      #   Math.sqrt(GSL::M_PI) * (value / GSL::M_E)**value \
+      #     * (((8 * value + 4) * value + 1) * value + 1.0 / 30.0)**(1.0 / 6.0)
+      # end
+    end
+  end
+end
